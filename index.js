bc8e69376e63310f0bac7991d50c86d244e57b86
Ramiz Ali <ramiz.apk7@gmail.com>
2023-01-02 01:44:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+
+// const gifts = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(gifts,text) {
+  const newGift=[];
+  for (let i = 0; i < gifts.length; i++) {
+    newGift[i]=`Thank you, ${gifts[i]}, for the wonderful ${text} gift!`;
+  }
+
+  return newGift;
+}
+
+function countDown(number){
+  let counter = 0;
+  while (counter <= number) {
+    console.log(number--);
+  }
+}
+
+
+
